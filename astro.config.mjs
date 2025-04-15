df9c865b68bb1232da66dde8498c18435050e9e0
juanjzb <zb.juanjose@gmail.com>
2025-04-14 22:37:26 -0600

    first version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+import path from 'path'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@layouts': path.resolve('./src/layouts'),
+        '@assets': path.resolve('./src/assets'),
+        '@components': path.resolve('./src/components')
+      }
+    }
+  }
+})
